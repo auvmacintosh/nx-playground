2c04ee7b3c6ca02a7bdbc3542bad3c7e80979db0
Nathan Wang <nathanbotinker@gmail.com>
2021-09-29 22:14:44 +1300

    build: add storybook

---
+++ libs/ui/Env/index.story.tsx
@@ -0,0 +1,31 @@
+import { Env } from '.'
+import type { Meta } from '@storybook/react'
+
+// export const Default = (): JSX.Element => <Env />
+export const Default = (): JSX.Element => <div>abc</div>
+
+const meta: Meta = {
+  component: Env,
+  title: 'abc',
+  // parameters: {
+  //   actions: {
+  //     argTypes: 'onClick',
+  //     handles: ['click'],
+  //   },
+  //   layout: 'fullscreen',
+  // },
+  // argTypes: {
+  //   onClick: { table: { disable: true } },
+  // },
+  // decorators: [
+  //   (Story: () => JSX.Element): JSX.Element => {
+  //     return (
+  //       <div style={wrapperStyle}>
+  //         <Story />
+  //       </div>
+  //     )
+  //   },
+  // ],
+}
+
+export default meta
