44d7443ffe5a8df9498dd5cb30139c81543b1eb0
Nathan Wang <nathanbotinker@gmail.com>
2021-10-01 22:21:13 +1300

    build: add webpack bundle analyzer

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -1,15 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
+
+module.exports = withNx({
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (process.env.ANALYZE) {
+     config.plugins.push(
+       new BundleAnalyzerPlugin({
+         analyzerMode: 'server',
+         analyzerPort: isServer ? 8888 : 8889,
+         openAnalyzer: true,
+       })
+     )
+    }
+    return config
+  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-};
-
-module.exports = withNx(nextConfig);
+})
